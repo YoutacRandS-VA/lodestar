1bf1afc9b7bd316213d50269934bdf893336d7d5
matthewkeil <me@matthewkeil.com>
2024-02-27 13:05:48 +0800

    feat: implement multiplyBy in chain/bls

--- packages/beacon-node/src/chain/bls/multithread/utils.ts
+++ packages/beacon-node/src/chain/bls/multithread/utils.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import bls from "@chainsafe/bls";
 import {CoordType, SignatureSet} from "@chainsafe/bls/types";
 import {WorkRequestSet, WorkResultError} from "./types.js";
@@ -39,4 +40,17 @@ export function getJobResultError(jobResult: WorkResultError | null, i: number):
   const workerError = jobResult ? Error(jobResult.error.message) : Error(`No jobResult for index ${i}`);
   if (jobResult?.error?.stack) workerError.stack = jobResult.error.stack;
   return workerError;
+}
+
+/**
+ * `rand` must not be exactly zero. Otherwise it would allow the verification of invalid signatures
+ * See https://github.com/ChainSafe/blst-ts/issues/45
+ */
+export function randomBytesNonZero(bytesCount: number): Buffer {
+  const rand = crypto.randomBytes(bytesCount);
+  for (let i = 0; i < bytesCount; i++) {
+    if (rand[0] !== 0) return rand;
+  }
+  rand[0] = 1;
+  return rand;
 }
